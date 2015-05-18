557dcad5aa5c4a61e99da900956f8eaeff137bd9
Ricky Davila <richardadavila@gmail.com>
2015-05-18 14:12:23 -0500

    New look

--- js/application.js
+++ js/application.js
@@ -58,5 +58,31 @@
 		}
 	}
 
+  // Send message
+  var contactFrom = document.getElementById('contact-form');
+  var contactSuccess = document.getElementById('contact-success');
+  var contactError = document.getElementById('contact-error');
+  var sendBtn = document.getElementById('send-button');
+  var onMessageComplete = function(error) {
+    sendBtn.disabled = false;
+    if (error) {
+      contactError.innerHTML = 'Sorry. Could not send message.';
+    } else {
+      contactSuccess.innerHTML = "Message has been sent.";
+      // hide the form
+      contactFrom.style.display = 'none';
+    }
+  };
+  function sendMessage(formObj) {
+    // Store emails to firebase
+    var myFirebaseRef = new Firebase("https://yourappname.firebaseio.com/messages");
+    myFirebaseRef.push({
+      name: formObj.name.value,
+      email: formObj.email.value,
+      message: formObj.message.value
+    }, onMessageComplete);
+    sendBtn.disabled = true;
+    return false;
+  }
 
 })(jQuery);
