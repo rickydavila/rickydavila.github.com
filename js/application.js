d6215bb7adcfd734e9ef025831bf949983caab1a
Ricky Davila <richardadavila@gmail.com>
2015-05-18 15:44:32 -0500

    Email functionality working.

--- js/application.js
+++ js/application.js
@@ -1,3 +1,8 @@
+function isValidEmailAddress(emailAddress) {
+  var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+  return pattern.test(emailAddress);
+};
+
 (function($) {
 
     // Switches
@@ -31,7 +36,7 @@
 		position: 'vertical-bottom'
 	});
 
-	// Button Animations
+/*	// Button Animations
 	var animateButtons = Array.prototype.slice.call( document.querySelectorAll( '.btn-animate' ) ),
 		totalButtons = animateButtons.length;
 		animateButtons.forEach( function( el, i ) {
@@ -56,33 +61,45 @@
 			classie.add( this, activatedClass );
 			setTimeout( function() { classie.remove( self, activatedClass ) }, 1500 );
 		}
-	}
+	}*/
 
   // Send message
-  var contactFrom = document.getElementById('contact-form');
+/*  var contactFrom = document.getElementById('contact-form');
   var contactSuccess = document.getElementById('contact-success');
   var contactError = document.getElementById('contact-error');
-  var sendBtn = document.getElementById('send-button');
+  var sendBtn = document.getElementById('send-button');*/
   var onMessageComplete = function(error) {
-    sendBtn.disabled = false;
+    //sendBtn.disabled = false;
     if (error) {
-      contactError.innerHTML = 'Sorry. Could not send message.';
+      //sendBtn.disabled = false;
+      //contactError.innerHTML = 'Sorry. Could not send message.';
+      $('#warning-row').show();
+      $('#send-button').removeClass('btn-animate-error btn-animate-success').addClass('btn-animate-error');
     } else {
-      contactSuccess.innerHTML = "Message has been sent.";
+      //contactSuccess.innerHTML = "Message has been sent.";
       // hide the form
-      contactFrom.style.display = 'none';
+      $('#send-button').removeClass('btn-animate-error').addClass('btn-animate-success');
+      $('#warning-row').hide();
+      $('#email-form-group').hide();
+      $('#company-form-group').hide();
+      $('#thankyou-form-group').show().addClass('bounceInLeft');
+      //console.log('All good');
     }
   };
-  function sendMessage(formObj) {
-    // Store emails to firebase
-    var myFirebaseRef = new Firebase("https://yourappname.firebaseio.com/messages");
-    myFirebaseRef.push({
-      name: formObj.name.value,
-      email: formObj.email.value,
-      message: formObj.message.value
-    }, onMessageComplete);
-    sendBtn.disabled = true;
-    return false;
-  }
 
+  $('#send-button').click(function(event) {
+    event.preventDefault();
+    var email = $('#form-email').val();
+    var company = $('#form-company').val();
+    if(isValidEmailAddress(email) && company != '') {
+      var myFirebaseRef = new Firebase("https://boiling-fire-7840.firebaseio.com/");
+      myFirebaseRef.push({
+        company: company,
+        email: email
+      }, onMessageComplete);
+    } else {
+      $('#send-button').addClass('btn-result-error').addClass('btn-animate-error');
+      $('#warning-row').show();
+    }
+  });
 })(jQuery);
